602375a9940495969ab44abf4ab2331bc272d648
Chris Herbert <herbert@bermanco.com>
2017-04-25 22:02:10 -0400

    Enabled updates and file modifications

--- config/application.php
+++ config/application.php
@@ -71,9 +71,9 @@ define('NONCE_SALT', env('NONCE_SALT'));
 /**
  * Custom Settings
  */
-define('AUTOMATIC_UPDATER_DISABLED', true);
+// define('AUTOMATIC_UPDATER_DISABLED', true);
+// define('DISALLOW_FILE_EDIT', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
-define('DISALLOW_FILE_EDIT', true);
 
 /**
  * Bootstrap WordPress
